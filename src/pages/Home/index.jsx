3882e7374f1840a4d0eb1f1a1165dc45e8b4b0e9
quyen <quocquyen546@gmail.com>
2025-04-09 13:03:40 +0700

    fix 2

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,27 +6,22 @@ import DuoiEvent from "../../component/DuoiEvent";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  
+  
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await fetch(
-          "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
-          {
-            method: "GET",
-            headers: {
-              accept: "application/json",
-              Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`, // Dùng API Key từ env
-            },
-          }
-        );
-
-        const data = await response.json();
-        setMovies(data.results || []); // Cập nhật danh sách phim
-      } catch (error) {
-        console.error("Lỗi khi lấy dữ liệu phim:", error);
-      }
-    };
+      // Gọi API khi component mount
+      const fetchMovies = async () => {
+        try {
+          const res = await fetch("http://192.168.50.254:9091/api/movies/listMovie");
+          const data = await res.json();
+          // Lọc phim đang chiếu
+          const nowShowingMovies = data.filter((movie) => movie.isNowShowing);
+          setMovies(nowShowingMovies);
+        } catch (error) {
+          console.error("Lỗi khi gọi API:", error);
+        }
+      };
 
     fetchMovies();
   }, []);
